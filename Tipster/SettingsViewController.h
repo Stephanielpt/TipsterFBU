7afff9b7d6cb2fbf7dd5e1153de2a5c76b3ca598
Stephanie Lampotang <slampotang@fb.com>
2018-06-26 16:41:04 -0700

    working w slider but about to add pics

---
+++ Tipster/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Tipster
+//
+//  Created by Stephanie Lampotang on 6/26/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingsViewController : NSObject
+
+@end
---
+++ Tipster/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  Tipster
+//
+//  Created by Stephanie Lampotang on 6/26/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
